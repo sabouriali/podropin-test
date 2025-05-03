79d63771961bd42f879955ac44db4f75c24d0204
Ali <sabouri.ali@outlook.com>
2025-05-03 20:04:24 +0330

    map ir removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,25 @@
 import { useNavigate } from "react-router";
 
-import MapComp from "../components/MapComp";
+// import MapComp from "../components/MapComp";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="p-4">
-      <div className="flex items-center gap-4">
-        <button
-          onClick={() => navigate("/login")}
-          className="px-4 py-3 bg-blue-700 hover:bg-blue-800 rounded-lg text-white cursor-pointer"
-        >
-          ورود / ثبت نام
-        </button>
+    <div className="flex items-center gap-4 p-4">
+      <button
+        onClick={() => navigate("/login")}
+        className="px-4 py-3 bg-blue-700 hover:bg-blue-800 rounded-lg text-white cursor-pointer"
+      >
+        ورود / ثبت نام
+      </button>
 
-        <button
-          onClick={() => navigate("/shipping")}
-          className="px-4 py-3 bg-blue-700 hover:bg-blue-800 rounded-lg text-white cursor-pointer"
-        >
-          حساب کاربری
-        </button>
-      </div>
-
-      <div className="w-full h-[calc(100vh-6rem)] mt-4 relative rounded-2xl">
-        <MapComp />
-      </div>
+      <button
+        onClick={() => navigate("/shipping")}
+        className="px-4 py-3 bg-blue-700 hover:bg-blue-800 rounded-lg text-white cursor-pointer"
+      >
+        حساب کاربری
+      </button>
     </div>
   );
 }
